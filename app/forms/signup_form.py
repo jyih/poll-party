743ef7ccec53cc9f3470b91392a078c220f701e4
jojo <johnathanyih@gmail.com>
2021-09-01 11:12:31 -0700

    updated sign up validation

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -22,6 +22,16 @@ def username_exists(form, field):
 
 class SignUpForm(FlaskForm):
     username = StringField(
-        'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
+        'username',
+        validators=[DataRequired(),
+        username_exists])
+    email = StringField(
+        'email',
+        validators=[DataRequired(),
+        user_exists,
+        # Email()
+    ])
+    password = StringField(
+        'password',
+        validators=[DataRequired()
+    ])
