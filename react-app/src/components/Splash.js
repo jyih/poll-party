61b4ade3688e2eef1e1e8bac244144c57c4f340d
jojo <johnathanyih@gmail.com>
2021-09-01 16:32:37 -0700

    more styling

--- react-app/src/components/Splash.js
+++ react-app/src/components/Splash.js
@@ -3,14 +3,16 @@ import React from 'react';
 const Splash = () => {
 
   return (
-    <iframe
-      title='splash-banner'
-      src="https://ntmaker.gfto.ru/newneontexten/?image_height=200&image_width=600&image_font_shadow_width=30&image_font_size=111&image_background_color=03045E&image_text_color=FDFD96&image_font_shadow_color=5E89FF&image_url=https://cdn.mos.cms.futurecdn.net/rumru4mYc6NnKVY9rLeUs8-970-80.jpg.webp&image_text=Poll Party&image_font_family=Nickainley&"
-      frameborder='no'
-      scrolling='no'
-      width="600"
-      height="200">
-    </iframe>
+    <>
+      {/* <iframe
+        title='splash-banner'
+        src="https://ntmaker.gfto.ru/newneontexten/?image_height=200&image_width=600&image_font_shadow_width=30&image_font_size=111&image_background_color=03045E&image_text_color=FDFD96&image_font_shadow_color=5E89FF&image_url=https://cdn.mos.cms.futurecdn.net/rumru4mYc6NnKVY9rLeUs8-970-80.jpg.webp&image_text=Poll Party&image_font_family=Nickainley&"
+        frameBorder='no'
+        scrolling='no'
+        width="600"
+        height="200">
+      </iframe> */}
+    </>
   )
 }
 
