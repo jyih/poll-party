e66d736bef04adc432a017962c9ad437587ce1fe
jojo <johnathanyih@gmail.com>
2021-08-31 21:23:36 -0700

    styling updates

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
-import NavBar from './components/NavBar';
+import NavBar from './components/navigation/NavBar';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/user/UsersList';
 import User from './components/user/User';
@@ -11,7 +11,8 @@ import * as sessionActions from './store/session';
 import VoteForm from './components/poll/VoteForm';
 import PollResults from './components/poll/PollResults';
 import PollCreate from './components/poll/PollCreate';
-import PollEdit from './components/poll/PollEdit';
+import LandingPage from './components/LandingPage';
+// import PollEdit from './components/poll/PollEdit';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -31,38 +32,42 @@ function App() {
   return (
     <BrowserRouter>
       <NavBar />
-      <Switch>
-        <Route path='/login' exact={true}>
-          <LoginForm />
-        </Route>
-        <Route path='/sign-up' exact={true}>
-          <SignUpForm />
-        </Route>
-        <ProtectedRoute path='/users' exact={true} >
-          <UsersList />
-        </ProtectedRoute>
-        <ProtectedRoute path='/users/:userId' exact={true} >
-          <User />
-        </ProtectedRoute>
-        <ProtectedRoute path='/' exact={true} >
-          <h1>My Home Page</h1>
-        </ProtectedRoute>
+      <div className='content-container'>
 
-        <Route path='/create' exact={true}>
-          <PollCreate />
-        </Route>
-        <Route path='/polls/:pollId/' exact={true}>
-          <VoteForm />
-        </Route>
-        <Route path='/polls/:pollId/results' exact={true}>
-          <PollResults />
-        </Route>
-        {/* <Route path='/polls/:pollId/edit' exact={true}>
+        <Switch>
+          <Route path='/login' exact={true}>
+            <LoginForm />
+          </Route>
+          <Route path='/sign-up' exact={true}>
+            <SignUpForm />
+          </Route>
+          <ProtectedRoute path='/users' exact={true} >
+            <UsersList />
+          </ProtectedRoute>
+          <ProtectedRoute path='/users/:userId' exact={true} >
+            <User />
+          </ProtectedRoute>
+
+          <ProtectedRoute path='/' exact={true} >
+            <h1>My Home Page</h1>
+            <LandingPage />
+          </ProtectedRoute>
+
+          <Route path='/create' exact={true}>
+            <PollCreate />
+          </Route>
+          <Route path='/polls/:pollId/' exact={true}>
+            <VoteForm />
+          </Route>
+          <Route path='/polls/:pollId/results' exact={true}>
+            <PollResults />
+          </Route>
+          {/* <Route path='/polls/:pollId/edit' exact={true}>
           <PollEdit handleCancel={(e) => <Redirect to='/polls/:pollId' />}
           />
         </Route> */}
-
-      </Switch>
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 }
