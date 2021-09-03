0da10be535d252924c6897c8a10627228d2082ac
jojo <johnathanyih@gmail.com>
2021-09-02 17:44:08 -0700

    empty message corrected for user page

--- react-app/src/components/poll/PollList.js
+++ react-app/src/components/poll/PollList.js
@@ -4,18 +4,25 @@ import { NavLink } from "react-router-dom";
 const PollList = ({ polls }) => {
   return (
     <>
-      <div>
-        <strong>
-          List of Polls:
-        </strong>
-        {polls?.map((poll, i) => (
-          <div key={i}>
-            <NavLink to={`/polls/${poll.id}`}>
-              {poll.question}
-            </NavLink>
-          </div>
-        ))}
-      </div>
+      <strong>
+        List of Polls:
+      </strong>
+      {polls.length
+        ?
+        <div>
+          {polls?.map((poll, i) => (
+            <div key={i}>
+              <NavLink to={`/polls/${poll.id}`}>
+                {poll.question}
+              </NavLink>
+            </div>
+          ))}
+        </div>
+        :
+        <div>
+          (User has not created any polls)
+        </div>
+      }
     </>
   )
 };
