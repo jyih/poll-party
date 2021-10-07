ad774cc8b4a196c6db7aab54e482a5b92111e486
jojo <johnathanyih@gmail.com>
2021-10-07 02:16:34 -0700

    watchlist updates

--- app/api/watchlist_routes.py
+++ app/api/watchlist_routes.py
@@ -35,13 +35,14 @@ def watchlist_add():
     return watchlist.to_dict()
   return form.errors
 
-@watchlist_routes.route('/', methods=['DELETE'])
+@watchlist_routes.route('/<int:id>/<int:user_id>', methods=['DELETE'])
 # @login_required
-def watchlist_remove():
-  data = request.json
-  id = data['watchlist_id']
+def watchlist_remove(id, user_id):
+  # data = request.json
+  # id = data['watchlist_id']
   watchlist = Watchlist.query.get(id)
-  if watchlist['user_id'] == data['user_id']:
+  # if watchlist['user_id'] == data['user_id']:
+  if watchlist['user_id'] == user_id:
     db.session.delete(watchlist)
     db.session.commit()
     return {'message': f'Watchlist ${id} deleted'}
--- react-app/src/store/watchlist.js
+++ react-app/src/store/watchlist.js
@@ -1,3 +1,45 @@
 const SET_WATCHLIST = 'watchlist/SET_WATCHLIST';
 const UNSET_WATCHLIST = 'watchlist/UNSET_WATCHLIST';
 
+const set = (watchlist) => ({
+  type: SET_WATCHLIST,
+  payload: watchlist
+})
+
+export const createWatchlist = (payload) => async (dispatch) => {
+  const res = await fetch('/api/watchlists', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      'user_id': payload.user_id,
+      'poll_id': payload.poll_id
+    })
+  })
+  if (res.ok) {
+    const data = await res.json();
+    dispatch(set(data));
+    if (data.errors) {
+      return data.errors;
+    }
+    return data;
+  } else {
+    return ['An error occurred. Please try again.']
+  }
+}
+
+export const deleteWatchlist = (payload) => async (dispatch) => {
+  const res = await fetch(`/api/watchlists/${payload.id}`, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+  })
+  if (res.ok) {
+    const data = await res.json();
+    dispatch(unset())
+    if (data.errors) {
+      return data.errors;
+    }
+    return data;
+  } else {
+    return ['An error occurred. Please try again.']
+  }
+}
